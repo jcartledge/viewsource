a0ad235f0ff7a9347083c986865e261a11e22ae0
James Cartledge <jcartledge@gmail.com>
2012-06-15 15:44:21 +1000

    swapped highlighter for one that works

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 var get_url = require('request')
-  , hl = require('highlight').Highlight
+  , hljs = require('hljs')
 ;
 
 exports.index = function(req, res){
@@ -7,13 +7,15 @@ exports.index = function(req, res){
 };
 
 exports.main = function(req, res) {
-  var url=req.params[0];
+
+  var url = req.params[0];
   get_url(url, function(error, response, body) {
     if(!error && response.statusCode == 200) {
       res.render('main', {
         title: 'View source:' + url
-      , body: hl(body.replace('&', '&amp;').replace('<', '&lt;').replace('>', '&gt;'))
+      , body: hljs(body).value
       });
     }
   });
+
 };
